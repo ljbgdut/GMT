190358606d5e5b5707cafb807f43e472e219fc9b
Jinbo Li <906358129@qq.com>
2020-11-26 12:50:52 +0800

    master

--- bat/draw.bat
+++ bat/draw.bat
@@ -1,6 +1,12 @@
 gmt begin terr png
-    set input=D:\Sea_data_set\Topomodel\SIOtopo_20.1.nc
-    
-  
-    gmt grd2xyz %input% -R103/120/2/23 >SIO_103_120_2_23.txt
+    set input=D:\matlabprogram\crust1.0\uppercrust103_120_2_23.txt
+    set out=grd.grd
+    gmt surface %input% -R103/120/2/23 -I1d -G%out% 
+    gmt makecpt -T0/35/1 -Crainbow -Z
+    gmt basemap -Ba5f -R103/120/2/23 -JM7i
+    gmt grdimage %out% -I 
+    gmt coast -R103/120/2/23 -W1/0.5p,white,dashed -A100/0/1 -Da
+    gmt colorbar -Bxaf -DjMR+o-0.5i/0i+m -By+l"(km)" -S -I
+    REM gmt grd2xyz %input% -R100/130/0/30 > seabedm.grd
+
 gmt end show
