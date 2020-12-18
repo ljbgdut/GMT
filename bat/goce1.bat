655db637c952f8283b293177c6cc928737c0133c
Ljb <906358129@qq.com>
2020-12-18 11:15:49 +0800

    master

--- bat/goce1.bat
+++ bat/goce1.bat
@@ -1,5 +1,5 @@
 gmt begin tcvggSCS16km png E600
-    set input=D:\matlabprogram\mohoinvers\ETOPO10GOCE16km-ggt.txt -i0,1,3
+    set input=D:\matlabprogram\mohoinvers\tcgoce_ex10.txt -i0,1,2
     set out=grd.grd
     set seab=line\seabasinline.txt
     set fault=line\fault.txt
@@ -7,13 +7,13 @@ gmt begin tcvggSCS16km png E600
     set rig2=line\ridge2.txt
     set sd=line\subduction.txt
     set PN=line\pointname\pointname.txt
-    gmt set FONT 16p,Helvetica,black 
+    gmt set FONT 12p,Helvetica,black 
     
-    gmt xyz2grd %input% -R105/125/5/25 -I10m -G%out%
-    gmt grd2cpt %out% -R105/125/5/25 -CwaveletA -Z
-    REM gmt makecpt -T0/35/1 -Crainbow -Z
-    gmt basemap -BWSen -Ba5f -R105/125/5/25 -JM8i
-    gmt grdimage %out% -R105/125/5/25
+    gmt xyz2grd %input% -R95/135/-5/35 -I10m -G%out%
+    gmt grd2cpt %out% -R95/135/-5/35 -Crainbow -Z
+
+    gmt basemap -BWSen -Ba5f -R95/135/-5/35 -JM24i
+    gmt grdimage %out% -R95/135/-5/35
     gmt plot %seab% -W1.5p,white,dashed
     gmt plot %fault% -W2p,black,dashed
     gmt plot %rig1% -W2p,yellow,dashed
@@ -21,7 +21,7 @@ gmt begin tcvggSCS16km png E600
     gmt plot %sd% -W2.5p,139/0/0
     gmt text %PN% -F+f+a
 
-    gmt coast -R105/125/5/25 -W1/1.0p,black,dashed -A100/0/1 -Da
+    gmt coast -R95/135/-5/35 -W1/1.0p,black,dashed -A100/0/1 -Da
     gmt colorbar -C -DjBC+w7.5i/0.1i+o0i/-0.5i+m -Bx10f -By+l"(E)" -S -I
 
 gmt end show
